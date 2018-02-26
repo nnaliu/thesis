818a612be0539b40e973429eef886aa916f1a3e6
Anna Liu <yuhuiliu1@gmail.com>
2018-02-26 16:11:38 -0500

    adam 0.005

--- deeplearning-annaliu/utils.py
+++ deeplearning-annaliu/utils.py
@@ -34,7 +34,7 @@ def train(model, data_iter, val_iter, epochs, scheduler=None, grad_norm=5, has_f
     model.train()
     criterion = nn.CrossEntropyLoss()
     parameters = filter(lambda p: p.requires_grad, model.parameters())
-    optimizer = optim.Adam(parameters, lr=0.0001)
+    optimizer = optim.Adam(parameters, lr=0.005)
     # optimizer = optim.SGD(parameters, lr=0.1)
 
     counter = 0
