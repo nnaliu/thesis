3779cb340cd3acc491385c1ac02f5bd746abe0e1
Anna Liu <yuhuiliu1@gmail.com>
2018-02-26 15:45:27 -0500

    update

--- deeplearning-annaliu/model.py
+++ deeplearning-annaliu/model.py
@@ -150,7 +150,6 @@ class LSTMClassifier(nn.Module):
         hidden = self.init_hidden(batch_size)
         embeddings = self.dropout(self.embedding(inputs))
         embeddings1 = embeddings.view(len(inputs[0]), batch_size, -1)
-        # embeddings1 = embeddings.transpose(0, 1)
         output, hidden = self.lstm(embeddings1, hidden)
         # output: [seq_len x batch x hidden]
         output = self.hidden2label(self.dropout1(output[-1]))
@@ -193,8 +192,7 @@ class LSTMClassifierFeatures(nn.Module):
         batch_size = len(inputs)
         hidden = self.init_hidden(batch_size)
         embeddings = self.dropout(self.embedding(inputs))
-        # embeddings1 = embeddings.view(len(inputs[0]), batch_size, -1)
-        embeddings1 = embeddings.transpose(0, 1)
+        embeddings1 = embeddings.view(len(inputs[0]), batch_size, -1)
         output, hidden = self.lstm(embeddings1, hidden)
         # output: [seq_len x batch x hidden]
         output = output[-1].type(torch.FloatTensor).cuda() if USE_CUDA else output[-1].type(torch.FloatTensor)
