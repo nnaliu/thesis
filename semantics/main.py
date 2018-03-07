a6693afd1df1094b86a06e9b8c88a303517880da
Anna Liu <yuhuiliu1@gmail.com>
2018-03-07 02:38:56 -0500

    new changes

--- semantics/main.py
+++ semantics/main.py
@@ -16,18 +16,25 @@ with open('cache/all_tweets.csv', 'r') as f:
 
 seq = seq[0]
 
-my_model = Word2Vec(seq, size=300, window=5, min_count=1, workers=4)
-print(my_model)
-words = list(my_model.wv.vocab)
-print("Num words:", len(words))
-my_model.wv.save_word2vec_format('my_model.bin')
+my_model_filename = 'my_model.bin'
+if os.path.exists(my_model_filename):
+	my_model = KeyedVectors.load_word2vec_format(my_model_filename, binary=True)
+else:
+	my_model = Word2Vec(seq, size=300, window=5, min_count=1, workers=4)
+	print(my_model)
+	words = list(my_model.wv.vocab)
+	print("Num words:", len(words))
+	my_model.wv.save_word2vec_format(my_model_filename)
 
-glove_filename = '../deeplearning-annaliu/.vector_cache/glove.840B.300d.txt'
-gmodel = KeyedVectors.load_word2vec_format(glove_filename, binary=True)
-word2vec_output = 'glove.840B.300d.txt.word2vec'
-glove2word2vec(glove_filename, word2vec_output)
+if os.path.exists('my_model_aligned.bin'):
+	my_model_aligned = KeyedVectors.load_word2vec_format('my_model_aligned.bin', binary=True)
+else:
+	glove_filename = '../deeplearning-annaliu/.vector_cache/glove.840B.300d.txt'
+	gmodel = KeyedVectors.load_word2vec_format(glove_filename, binary=False)
+	word2vec_output = 'glove.840B.300d.txt.word2vec'
+	glove2word2vec(glove_filename, word2vec_output)
 
-gmodel = KeyedVectors.load_word2vec_format(word2vec_output, binary=False)
-my_model_aligned = align.smart_procrustes_align_gensim(gmodel, my_model)
-my_model_aligned.wv.save_word2vec_format('my_model_aligned.bin')
+	gmodel = KeyedVectors.load_word2vec_format(word2vec_output, binary=False)
+	my_model_aligned = align.smart_procrustes_align_gensim(gmodel, my_model)
+	my_model_aligned.wv.save_word2vec_format('my_model_aligned.bin')
 
