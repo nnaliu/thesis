6f88d961b4c1d649c776893518ff3f058e814bc8
Anna Liu <yuhuiliu1@gmail.com>
2018-03-15 13:12:56 -0400

    hi

--- deeplearning-annaliu/data_handler.py
+++ deeplearning-annaliu/data_handler.py
@@ -94,7 +94,10 @@ def get_dataset(tweets, lower=False, vectors=None, n_folds=10, seed=42):
     ]
 
     tweets_arr = np.array(tweets)
-    all_tweets = data.Dataset(tweets_arr, fields=fields)
+    all_tweets = data.TabularDataset(
+        path='cache/tweets_data.csv', format='csv', skip_header=True,
+        fields=fields
+    )
     tweet.build_vocab(all_tweets)
     label.build_vocab(all_tweets)
 
