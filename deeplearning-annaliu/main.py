682fb035cfcced8f1a07b5b92da8ab49a92b3ee3
Anna Liu <yuhuiliu1@gmail.com>
2018-03-15 15:43:18 -0400

    hi

--- deeplearning-annaliu/main.py
+++ deeplearning-annaliu/main.py
@@ -54,11 +54,10 @@ if args.use:
         print("USING CUDA")
         model = model.cuda()
 
-    for fold(train, val) in enumerate(train_val_generator):
+    for fold, (train, val) in enumerate(train_val_generator):
         print("FOLD " + str(fold))
         train_iter, val_iter = data_handler.get_bucket_iterators((train, val), args.batch_size)
         p, r, f1, p1, r1, f11 = utils.evaluate(model, val_iter, has_features=True)
-
         p_avg += p
         r_avg += r
         f1_avg += f1
@@ -82,7 +81,6 @@ else:
 
     for fold, (train, val) in enumerate(train_val_generator):
         print("FOLD " + str(fold))
-
         train_iter, val_iter = data_handler.get_bucket_iterators((train, val), args.batch_size)
 
         if args.model == 'CNN':
@@ -110,6 +108,17 @@ else:
         r1_avg += r1
         f11_avg += f11
 
+    # Saving Model
+    if args.model == "CNN":
+        filename = 'cnn_model.sav'
+    elif args.model == "CNNFeatures":
+        filename = 'cnn_model_features.sav'
+    elif args.model == "LSTM":
+        filename = 'lstm_model.sav'
+    elif args.model == "LSTMFeatures":
+        filename = 'lstm_model_features.sav'
+    torch.save(model.state_dict(), filename)
+
 print('WEIGHTED RESULTS')
 print('average precision is ' + str(p_avg/N_FOLDS))
 print('average recall is ' + str(r_avg/N_FOLDS))
@@ -119,17 +128,6 @@ print('MICRO RESULTS')
 print('average precision is ' + str(p1_avg/N_FOLDS))
 print('average recall is ' + str(r1_avg/N_FOLDS))
 print('average f1 is ' + str(f11_avg/N_FOLDS))
-
-# Saving Model
-if args.model == "CNN":
-    filename = 'cnn_model.sav'
-elif args.model == "CNNFeatures":
-    filename = 'cnn_model_features.sav'
-elif args.model == "LSTM":
-    filename = 'lstm_model.sav'
-elif args.model == "LSTMFeatures":
-    filename = 'lstm_model_features.sav'
-torch.save(model.state_dict(), filename)
 
 """
 GuidedBackProp Saliency Analysis
