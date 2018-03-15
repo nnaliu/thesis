67bb1e29d1e2c834b27c3f990744e7c81b4a3403
Anna Liu <yuhuiliu1@gmail.com>
2018-03-15 15:40:48 -0400

    hi

--- deeplearning-annaliu/main.py
+++ deeplearning-annaliu/main.py
@@ -25,6 +25,7 @@ parser = argparse.ArgumentParser(description='Hate Speech Classification')
 parser.add_argument('--model', type=str, help='type of model')
 parser.add_argument('--batch_size', type=int, default=128)
 parser.add_argument('--epochs', type=int, default=50)
+parser.add_argument('--use', type=str, default=None, help='use a pretrained model')
 args = parser.parse_args()
 
 # Need to figure out how to not have headers writing to file in middle (Ctrl+F 'retweet_count')
@@ -44,48 +45,70 @@ vectors = Vectors('wiki.simple.vec', url=url)
 train_val_generator, vocab_size, tweet_vocab = data_handler.get_dataset(tweet_data, lower=True, vectors=vectors, n_folds=N_FOLDS, seed=42)
 print("Vocab size ", vocab_size)
 
-if args.model == 'CNN':
-    model = model.CNNClassifier(model='multichannel', vocab_size=vocab_size, class_number=2)
-elif args.model == 'CNNFeatures':
-    model = model.CNNClassifierFeatures(model='multichannel', vocab_size=vocab_size, class_number=2)
-elif args.model == 'LSTM':
-    model = model.LSTMClassifier(256, 300, vocab_size, 2, n_layers=4, batch_sz=args.batch_size) # embedding dim, hidden dim, vocab_size, label_size
-elif args.model == 'LSTMFeatures':
-    model = model.LSTMClassifierFeatures(256, 300, vocab_size, 2, n_layers=4, batch_sz=args.batch_size) # embedding dim, hidden dim, vocab_size, label_size
+p_avg, r_avg, f1_avg = 0., 0., 0.
+p1_avg, r1_avg, f11_avg = 0., 0., 0.
 
-if USE_CUDA:
-    print("USING CUDA")
-    model = model.cuda()
+if args.use:
+    model = load_state_dict(torch.load(args.use))
+    if USE_CUDA:
+        print("USING CUDA")
+        model = model.cuda()
 
-for fold, (train, val) in enumerate(train_val_generator):
-    train_iter, val_iter = data_handler.get_bucket_iterators((train, val), args.batch_size)
-    p_avg, r_avg, f1_avg = 0., 0., 0.
-    p1_avg, r1_avg, f11_avg = 0., 0., 0.
+    for fold(train, val) in enumerate(train_val_generator):
+        print("FOLD " + str(fold))
+        train_iter, val_iter = data_handler.get_bucket_iterators((train, val), args.batch_size)
+        p, r, f1, p1, r1, f11 = utils.evaluate(model, val_iter, has_features=True)
 
+        p_avg += p
+        r_avg += r
+        f1_avg += f1
+        p1_avg += p1
+        r1_avg += r1
+        f11_avg += f11
+
+else:
     if args.model == 'CNN':
-        utils.train(model, train_iter, val_iter, 10) # Change number of epochs later
-        p, r, f1, p1, r1, f11 = utils.evaluate(model, val_iter)
-        # print("Validation: ", utils.evaluate(model, val_iter))
-    elif args.model == "CNNFeatures":
-        utils.train(model, train_iter, val_iter, 10, has_features=True) # Change number of epochs later
-        p, r, f1, p1, r1, f11 = utils.evaluate(model, val_iter, has_features=True)
-
+        model = model.CNNClassifier(model='multichannel', vocab_size=vocab_size, class_number=2)
+    elif args.model == 'CNNFeatures':
+        model = model.CNNClassifierFeatures(model='multichannel', vocab_size=vocab_size, class_number=2)
     elif args.model == 'LSTM':
-        utils.train(model, train_iter, val_iter, 30)
-        # print("Validation: ", utils.evaluate(model, val_iter))
-        p, r, f1, p1, r1, f11 = utils.evaluate(model, val_iter)
-
+        model = model.LSTMClassifier(256, 300, vocab_size, 2, n_layers=4, batch_sz=args.batch_size) # embedding dim, hidden dim, vocab_size, label_size
     elif args.model == 'LSTMFeatures':
-        utils.train(model, train_iter, val_iter, 30, has_features=True)
-        # print("Validation: ", utils.evaluate(model, val_iter, has_features=True))
-        p, r, f1, p1, r1, f11 = utils.evaluate(model, val_iter, has_features=True)
+        model = model.LSTMClassifierFeatures(256, 300, vocab_size, 2, n_layers=4, batch_sz=args.batch_size) # embedding dim, hidden dim, vocab_size, label_size
 
-    p_avg += p
-    r_avg += r
-    f1_avg += f1
-    p1_avg += p1
-    r1_avg += r1
-    f11_avg += f11
+    if USE_CUDA:
+        print("USING CUDA")
+        model = model.cuda()
+
+    for fold, (train, val) in enumerate(train_val_generator):
+        print("FOLD " + str(fold))
+
+        train_iter, val_iter = data_handler.get_bucket_iterators((train, val), args.batch_size)
+
+        if args.model == 'CNN':
+            utils.train(model, train_iter, val_iter, 10) # Change number of epochs later
+            p, r, f1, p1, r1, f11 = utils.evaluate(model, val_iter)
+            # print("Validation: ", utils.evaluate(model, val_iter))
+        elif args.model == "CNNFeatures":
+            utils.train(model, train_iter, val_iter, 10, has_features=True) # Change number of epochs later
+            p, r, f1, p1, r1, f11 = utils.evaluate(model, val_iter, has_features=True)
+
+        elif args.model == 'LSTM':
+            utils.train(model, train_iter, val_iter, 30)
+            # print("Validation: ", utils.evaluate(model, val_iter))
+            p, r, f1, p1, r1, f11 = utils.evaluate(model, val_iter)
+
+        elif args.model == 'LSTMFeatures':
+            utils.train(model, train_iter, val_iter, 30, has_features=True)
+            # print("Validation: ", utils.evaluate(model, val_iter, has_features=True))
+            p, r, f1, p1, r1, f11 = utils.evaluate(model, val_iter, has_features=True)
+
+        p_avg += p
+        r_avg += r
+        f1_avg += f1
+        p1_avg += p1
+        r1_avg += r1
+        f11_avg += f11
 
 print('WEIGHTED RESULTS')
 print('average precision is ' + str(p_avg/N_FOLDS))
@@ -114,27 +137,29 @@ GuidedBackProp Saliency Analysis
 
 # filename = 'cnn_model.sav'
 # cnn = model.CNNClassifier(model='multichannel', vocab_size=vocab_size, class_number=2)
+
 if USE_CUDA:
     print("converting to cuda")
     model = model.cuda()
+
 # model.load_state_dict(torch.load(filename))
 
-counter = 0
-batch = next(iter(train_iter))
-text, label = utils.process_batch(batch)
-for text_i, label_i in zip(text, label):
-    # utils.saliency_map(model, text_i, label_i)
-    text_words = " ".join([tweet_vocab.vocab.itos[i.data[0]] for i in text_i])
-    print("TEXT: ", text_words)
-    print("LABEL: ", label_i)
-    GBP = utils.GuidedBackprop(model, text_i, label_i-1)
-    guided_grads = GBP.generate_gradients()
+# counter = 0
+# batch = next(iter(train_iter))
+# text, label = utils.process_batch(batch)
+# for text_i, label_i in zip(text, label):
+#     # utils.saliency_map(model, text_i, label_i)
+#     text_words = " ".join([tweet_vocab.vocab.itos[i.data[0]] for i in text_i])
+#     print("TEXT: ", text_words)
+#     print("LABEL: ", label_i)
+#     GBP = utils.GuidedBackprop(model, text_i, label_i-1)
+#     guided_grads = GBP.generate_gradients()
 
-    metadata = open('./data/metadata' + str(counter) + '.txt', 'w')
-    metadata.write(text_words)
-    metadata.write(str(label_i.data))
-    metadata.close()
+#     metadata = open('./data/metadata' + str(counter) + '.txt', 'w')
+#     metadata.write(text_words)
+#     metadata.write(str(label_i.data))
+#     metadata.close()
 
-    utils.save_saliency_map(counter, guided_grads, tweet_vocab, text_i, label_i)
-    counter += 1
+#     utils.save_saliency_map(counter, guided_grads, tweet_vocab, text_i, label_i)
+#     counter += 1
 
