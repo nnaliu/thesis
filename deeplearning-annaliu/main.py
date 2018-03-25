b707013173fcd44cd005cc534451dae974789e1c
Anna Liu <yuhuiliu1@gmail.com>
2018-03-25 14:45:17 -0400

    lstm

--- deeplearning-annaliu/main.py
+++ deeplearning-annaliu/main.py
@@ -119,7 +119,7 @@ elif args.model:
         model = get_model()
 
         if args.model == 'CNN' or args.model == 'CNNMulti' or args.model == 'LSTM':
-            utils.train(model, train_iter, val_iter, 30)
+            utils.train(model, train_iter, val_iter, 40)
             p, r, f1, p1, r1, f11 = utils.evaluate(model, val_iter)
         elif args.model == "CNNFeatures" or args.model == 'CNNMultiFeatures' or args.model == 'LSTMFeatures':
             utils.train(model, train_iter, val_iter, 30, has_features=True)
